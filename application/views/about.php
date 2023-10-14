cf069239016c13c52cd8fc4f0ae13a417e60b628
Ramdhandijaya <dijayaramdhan@gmail.com>
2023-10-14 21:51:35 +0700

    update about

---
+++ application/views/about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <style type="text/css"> 
+        .about-me {
+        background-color: burlywood;
+        padding: 40px;
+        text-align: center;
+      }
+
+      .about-me h2 {
+        margin-bottom: 20px;
+      }
+
+      .about-me .card {
+        width: 300px;
+        display: inline-block;
+        margin-right: 20px;
+        margin-bottom: 20px;
+      }
+
+      .about-me .card img {
+        width: 100%;
+        height: auto;
+      }
+      .card1 {
+        width: 300px;
+        display: inline-block;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        margin-top: -20%;
+      }
+
+      .about-me .card-body {
+        padding: 10px;
+      }
+
+      .about-me p {
+        text-align: center;
+      }
+    </style>
+</head>
+<body>
+<section id="about" class="about-me">
+      <h2>About Hotel</h2>
+      <p>
+        Rumah Sakit Lira Medika mengutamakan keselamatan pasien dan mutu
+        pelayanan yang didukung dengan fasilitas lengkap dan modern serta tenaga
+        medis yang profesional dibidangnya. Rumah Sakit Lira Medika beroperasi
+        sejak 5 Juni 2014 dan berada di atas tanah seluas ± 2 Hektar. Berlokasi
+        di Jalan Syech Quro No 14 Lamaran, Karawang, Jawa Barat.
+      </p>
+      <div class="card">
+        <img src="img/klinik(1).jpg" alt="Gambar 1" />
+        <div class="card-body">
+          <h5 class="card-title">Ruang konsultasi keluhan</h5>
+          <p class="card-text">Tempat Kami</p>
+        </div>
+      </div>
+      <div class="card">
+        <img src="img/klinik2.jpg" alt="Gambar 2" />
+        <div class="card-body">
+          <h5 class="card-title">Ruang konsultasi keluhan</h5>
+          <p class="card-text">
+            untuk mengetahui informasi keluhan dari pasien
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img src="img/klinik3.jpg" alt="Gambar 3" />
+        <div class="card-body">
+          <h5 class="card-title">Ruang Periksa</h5>
+          <p class="card-text">pemeriksaan setelah keluhan</p>
+        </div>
+      </div>
+    </section>
+
+</body>
+</html>
